69bf2d966ed607cd1ab774b04883e8ed0165fcef
Mason Embry <mason@embrycode.com>
2016-11-12 12:48:20 -0500

    User story 2 working.

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,25 @@
 'use strict';
 
-var gulp = require('gulp'),
-  concat = require('gulp-concat'),
-  uglify = require('gulp-uglify'),
-    sass = require('gulp-sass'),
-    maps = require('gulp-sourcemaps');
+var    gulp = require('gulp'),
+     concat = require('gulp-concat'),
+     uglify = require('gulp-uglify'),
+  uglifycss = require('gulp-uglifycss'),
+     rename = require('gulp-rename'),
+       sass = require('gulp-sass'),
+       maps = require('gulp-sourcemaps'),
+        del = require('del');
 
 var options = {
   src: 'src',
   dist: 'dist'
 };
+
+/**
+ * Deletes dist and src/css directories
+ */
+gulp.task('clean', function() {
+  del([options.dist, options.src + '/css']);
+});
 
 gulp.task('scripts', function() {
   return gulp.src([
@@ -23,14 +33,17 @@ gulp.task('scripts', function() {
   .pipe(gulp.dest(options.dist + '/js'));
 });
 
-gulp.task('styles', function() {
-  return gulp.src([
-    options.src + '/js/circle/*.js',
-    options.src + '/js/*.js'
-  ])
+gulp.task('compileSass', function() {
+  return gulp.src(options.src + '/sass/global.scss')
   .pipe(maps.init())
-  .pipe(concat('all.min.js'))
-  .pipe(uglify())
+  .pipe(sass())
   .pipe(maps.write('./'))
-  .pipe(gulp.dest(options.dist));
+  .pipe(gulp.dest(options.src + '/css'));
+});
+
+gulp.task('styles', ['compileSass'], function() {
+  return gulp.src(options.src + '/css/global.css')
+  .pipe(uglifycss())
+  .pipe(rename('all.min.css'))
+  .pipe(gulp.dest(options.dist + '/css'));
 });
